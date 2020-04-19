833a180ddb1e06bbe8ca9bc256e5f4b5fbd8ee00
= <=>
2020-04-19 11:21:16 -0700

    finished button UI

---
+++ stories/CindyComponents.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Buttons from '../comps/Buttons';
+
+export default {
+    title: "Cindy's Components",
+    component: Buttons
+}
+
+export const LinktButton = () =>
+<Buttons/>
