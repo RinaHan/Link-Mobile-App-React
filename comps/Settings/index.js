b735bc8fe875f968039712f8239ca2050c550c35
= <=>
2020-04-19 00:15:40 -0700

    settings

--- comps/Settings/index.js
+++ comps/Settings/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './settings.css';
 
-const Settings = ({}) => <div>
+const defaultImg = require('./default.png');
 
-
+const Settings = ({phone_number}) => <div id="phone_box">
+<div id="phone_text">Phone Number</div>
+<div>{phone_number}</div>
+<img src={defaultImg} style={{width:"13px", height:"13px", opacity:"50%"}}
+/>
 </div>;
 
-Settings, defaultProps = {
-
+Settings. defaultProps = {
+    phone_number: "7777777777",
 }
 
 export default Settings;
