e2b672ad256393690b0003091b941db90010ca41
= <=>
2020-04-19 02:31:37 -0700

    done

--- stories/Components.stories.js
+++ stories/Components.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Settings from '../comps/Settings';
 import Profile from '../comps/Profile';
+import Dating_Profile from '../comps/Dating_Profile';
 
 
 export default {
@@ -14,4 +15,8 @@ export const settings = () =><div>
 
 export const profile = () =><div>
 <Profile />
+</div>
+
+export const dating_profile = () =><div>
+<Dating_Profile />
 </div>
