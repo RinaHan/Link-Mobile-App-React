e98c3a6d4bdde26d3df94d909f73324b2bd48d4d
= <=>
2020-04-22 01:59:22 -0700

    change font syntax(bug)

--- comps/Dating_Profile/index.js
+++ comps/Dating_Profile/index.js
@@ -11,11 +11,11 @@ const Dating_Profile = ({ name, age, bio, location, from, major, university, hig
         <a class="con name">{name}{age}</a>
         <a id="bio">{bio}</a>
         
-        <span><img src={defaultImg1} /><a>{location}</a></span>
-        <span><img src={defaultImg2} /><a>{from}</a></span>
-        <span><img src={defaultImg3} /><a>{major}</a></span>
-        <span><img src={defaultImg4} /><a>{university}</a></span>
-        <span><img src={defaultImg4} /><a>{highschool}</a></span>
+        <div class="datingdiv"><img src={defaultImg1} /><a class="datingtext">{location}</a></div>
+        <div class="datingdiv"><img src={defaultImg2} /><a class="datingtext">{from}</a></div>
+        <div class="datingdiv"><img src={defaultImg3} /><a class="datingtext">{major}</a></div>
+        <div class="datingdiv"><img src={defaultImg4} /><a class="datingtext">{university}</a></div>
+        <div class="datingdiv"><img src={defaultImg4} /><a class="datingtext">{highschool}</a></div>
         
         <div id="button"><button>MESSAGE!</button></div>
     </div>;
