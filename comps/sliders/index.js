e6048b05cd8bcb3ffd4c452e11e95dd4caaa26ef
= <=>
2020-04-24 08:52:58 -0700

    profilesetup page edit

--- comps/sliders/index.js
+++ comps/sliders/index.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import './sliders.css';
 
-const Sliders = () => <div className="slider_bubble"> 
-    <div className="title_box">
-        <p className="title">Maximum Distance</p>
-            <p className="bubble"><span id="result">0</span><br/><span id="units">km</span></p>
+const Sliders = ({ text1, text2 }) =>
+    <div><div class="slider_bubble">
+        <div class="title_box">
+            <p class="title1">{text1}</p>
+            <p class="bubble">
+                <span id="result">0</span><br />
+                <span id="units">km</span></p>
+        </div>
+        <input type="range" id="slider" name="slider" min="0" max="150" step="1" onChange={showValue} />
     </div>
-    <input type="range" id="slider" name="slider" min="0" max="150" step="1" onChange={showValue}/>
-
-</div>
+    <div class="slider_bubble">
+        <div class="title_box">
+            <p class="title2">{text2}</p>
+            <p class="bubble">
+                <span id="result">18-34</span><br />
+    </p>
+        </div>
+        <input type="range" id="slider" name="slider" min="0" max="150" step="1" onChange={showValue} />
+    </div>
+    </div>
+    
 
 function showValue() {
     var input = document.getElementById("slider");
     var output = document.getElementById("result");
     output.innerHTML = input.value;
 
-    input.oninput = function() {
+    input.oninput = function () {
         output.innerHTML = this.value;
-        
 
-    // Show + after value when value = 10
-        if(this.value == 10){
+
+        // Show + after value when value = 10
+        if (this.value == 10) {
             output.innerHTML = this.value + "+";
         }
     }
+}
+
+Sliders.defaultProps = {
+    text1: "Maximum Distance",
+    text2: "Age Range"
 }
 
 export default Sliders;
